6ae9928ec919ee24d46e964293fbba88aedce8d6
GAS <nikitashaitor@yandex.ru>
2025-08-12 21:59:42 +0300

    Homework1.1 done

---
+++ src/main/java/ru/otus/java/basic/homework1/Homework1.java
@@ -0,0 +1,14 @@
+package ru.otus.java.basic.homework1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        greetings();
+    }
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+}
