1d5aff74e365de8eceb0c427ce2c17ee07d998b0
GAS <nikitashaitor@yandex.ru>
2025-08-12 22:07:27 +0300

    Homework1.2 done

--- src/main/java/ru/otus/java/basic/homework1/Homework1.java
+++ src/main/java/ru/otus/java/basic/homework1/Homework1.java
@@ -3,6 +3,7 @@ package ru.otus.java.basic.homework1;
 public class Homework1 {
     public static void main(String[] args) {
         greetings();
+        checkSign(1, 2, 3);
     }
 
     public static void greetings() {
@@ -10,5 +11,15 @@ public class Homework1 {
         System.out.println("World");
         System.out.println("from");
         System.out.println("Java");
+    }
+
+    public static void checkSign(int a, int b, int c) {
+        int sum = a + b + c;
+
+        if (sum >= 0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+
     }
 }
