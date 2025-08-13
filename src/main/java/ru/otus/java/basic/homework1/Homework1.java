cfe07b632bb0026237fcf9cefe93ceda2f775d1d
GAS <nikitashaitor@yandex.ru>
2025-08-13 13:34:06 +0300

    Homework1 Добавил выбор в метод main (выбор заданий)

--- src/main/java/ru/otus/java/basic/homework1/Homework1.java
+++ src/main/java/ru/otus/java/basic/homework1/Homework1.java
@@ -1,12 +1,25 @@
 package ru.otus.java.basic.homework1;
+import java.util.Scanner;
 
 public class Homework1 {
     public static void main(String[] args) {
-        greetings();
-        checkSign(1, 2, 3);
-        selectColor(5);
-        compareNumbers(7, 2);
-        addOrSubtractAndPrint(14, 2, true);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число от 1 до 5\n1. Метод greetings.\n2. Метод checkSign.\n3. Метод selectColor\n4. Метод compareNumbers\n5. Метод addOrSubtractAndPrint");
+        int choise = scanner.nextInt();
+            if (choise == 1) {
+                greetings();
+            } else if (choise == 2) {
+                checkSign(1,2,3);
+            } else if (choise == 3) {
+                selectColor(5);
+            } else if (choise == 4) {
+                compareNumbers(7, 2);
+            } else if (choise == 5) {
+                addOrSubtractAndPrint(14,2, true);
+            } else {
+                System.out.println("Число введено не верно");
+            }
     }
 
     public static void greetings() {
@@ -19,11 +32,11 @@ public class Homework1 {
     public static void checkSign(int a, int b, int c) {
         int sum = a + b + c;
 
-        if (sum >= 0)
+        if (sum >= 0) {
             System.out.println("Сумма положительная");
-        else
+        } else {
             System.out.println("Сумма отрицательная");
-
+        }
     }
 
     public static void selectColor(int data) {
