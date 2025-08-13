6fa062ece13862064f88acee88adf47e1318a117
GAS <nikitashaitor@yandex.ru>
2025-08-13 14:21:54 +0300

    Homework1 Добавил рандома для всех заданий

--- src/main/java/ru/otus/java/basic/homework1/Homework1.java
+++ src/main/java/ru/otus/java/basic/homework1/Homework1.java
@@ -5,18 +5,24 @@ public class Homework1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        // Тут прошу ввести выбор задания (с пояснением) для облегчения поиска и выбора
         System.out.println("Введите целое число от 1 до 5\n1. Метод greetings.\n2. Метод checkSign.\n3. Метод selectColor\n4. Метод compareNumbers\n5. Метод addOrSubtractAndPrint");
         int choise = scanner.nextInt();
             if (choise == 1) {
                 greetings();
             } else if (choise == 2) {
-                checkSign(1,2,3);
+                // Для того что бы был рандом от -10 до 10 взял рандомные цифры от 0 до 20 и вычел 10
+                checkSign((int) (Math.random() * 21) - 10,(int) (Math.random() * 21) - 10,(int) (Math.random() * 21) - 10);
             } else if (choise == 3) {
-                selectColor(5);
+                // Взял для до 30. Т.к. По условиям задачи должно быть число больше 20
+                selectColor((int) (Math.random() * 31));
             } else if (choise == 4) {
-                compareNumbers(7, 2);
+                // Тут так же взял для того что бы было от -10 до 10
+                compareNumbers((int) (Math.random() * 21) - 10, (int) (Math.random() * 21) - 10);
             } else if (choise == 5) {
-                addOrSubtractAndPrint(14,2, true);
+                // Для initValue и delta взял числа от 0 до 10. Для boolean increment взял два числа 0 и 1. Затем сравнил его с 0. Если == 0, то true, иначе false
+                addOrSubtractAndPrint((int) (Math.random() * 11),(int) (Math.random() * 11), ((int) (Math.random() * 2)) == 0);
             } else {
                 System.out.println("Число введено не верно");
             }
