13b0ba904a5b09404ee8d0174133aa9cb8b6959f
GAS <nikitashaitor@yandex.ru>
2025-09-04 14:27:48 +0300

    Homework4.4 done

--- src/main/java/ru/otus/java/basic/MyApp.java
+++ src/main/java/ru/otus/java/basic/MyApp.java
@@ -2,10 +2,23 @@ package ru.otus.java.basic;
 
 public class MyApp {
     public static void main(String[] args) {
-        User User1 = new User("Nikita", "Shaitor", "Sergeevich", "19.10.2000", "nikitashaytor@yandex.ru");
-        User User2 = new User("Ivan", "Ivanov", "Ivanovich", "01.01.1990", "ivanovich@yandex.ru");
-        User1.info();
-        User2.info();
+        User[] users = new User[10];
 
+        users[0] = new User("Иван", "Иванов", "Иванович", "1980-01-01", "ivan@yandex.ru");   // Старше 40 лет
+        users[1] = new User("Петр", "Петров", "Петрович", "1990-02-02", "petr@yandex.ru");  // Младше 40 лет
+        users[2] = new User("Алексей", "Алексеев", "Алексеевич", "1975-03-03", "aleksey@yandex.ru"); // Старше 40 лет
+        users[3] = new User("Сергей", "Сергеев", "Сергеевич", "1985-04-04", "sergey@yandex.ru"); // Старше 40 лет
+        users[4] = new User("Дмитрий", "Дмитриев", "Дмитриевич", "1995-05-05", "dmitry@yandex.ru"); // Младше 40 лет
+        users[5] = new User("Михаил", "Михайлов", "Михайлович", "1970-06-06", "misha@yandex.ru"); // Старше 40 лет
+        users[6] = new User("Александр", "Александров", "Александрович", "1965-07-07", "sasha@yandex.ru"); // Старше 40 лет
+        users[7] = new User("Елена", "Иванова", "Дмитриевна", "1988-08-08", "elena@yandex.ru"); // Старше 40 лет
+        users[8] = new User("Анна", "Андреевна", "Андреевна", "1998-09-09", "anna@yandex.ru"); // Младше 40 лет
+        users[9] = new User("Борис", "Борисов", "Борисович", "1960-10-10", "boris@yandex.ru"); // Старше 40 лет
+
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].calculateAge() > 40) {
+                users[i].info();
+            }
+        }
     }
 }
--- src/main/java/ru/otus/java/basic/User.java
+++ src/main/java/ru/otus/java/basic/User.java
@@ -1,23 +1,29 @@
 package ru.otus.java.basic;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class User {
     public String firstName;
     public String lastName;
     public String middleName;
-    public String yearBirth;
+    public LocalDate yearBirth;
     public String email;
 
     public User(String firstName, String lastName, String middleName, String yearBirth, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.middleName = middleName;
-        this.yearBirth = yearBirth;
+        this.yearBirth = LocalDate.parse(yearBirth);
         this.email = email;
     }
 
+    public int calculateAge() {
+        return Period.between(yearBirth, LocalDate.now()).getYears(); // Используем библиотеку Java Time API
+    }
+
     public void info() {
-        System.out.println("ФИО: " + lastName + " " + firstName + " " + lastName);
-        System.out.println("Год рождения: " + yearBirth);
+        System.out.println("ФИО: " + lastName + " " + firstName + " " + middleName);
+        System.out.println("Возраст: " + calculateAge());
         System.out.println("E-mail: " + email + "\n");
     }
 }
