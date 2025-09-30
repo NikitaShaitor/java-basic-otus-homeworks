86e969810ecda79f67d84af3f1247e7a1a97927b
GAS <nikitashaitor@yandex.ru>
2025-09-30 18:02:17 +0300

    Homework10 done

--- src/main/java/ru/otus/java/basic/MyApp.java
+++ src/main/java/ru/otus/java/basic/MyApp.java
@@ -3,6 +3,18 @@ package ru.otus.java.basic;
 public class MyApp {
     public static void main(String[] args) {
 
+        PhoneBook book = new PhoneBook();
+
+        book.add("Иванов Иван Иванович", "+79211234567");
+        book.add("Иванова Анна Ивановна", "+79217654321");
+        book.add("Иванов Иван Иванович", "+79219876543"); // У Петра два номера
+
+        System.out.println(book.find("Иванов Иван Иванович"));
+        System.out.println(book.find("Иванова Анна Ивановна"));
+        System.out.println(book.find("Оборин Алексей Иванович"));
+
+        System.out.println(book.containsPhoneNumber("+79211234567"));
+        System.out.println(book.containsPhoneNumber("+79210000000"));
     }
 
 }
